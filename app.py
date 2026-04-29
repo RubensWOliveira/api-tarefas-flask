03cf9a64a187f55ffc0cc94ebf4e36489b5c1946
RubensWOliveira <wagner.xuxo@gmail.com>
2026-04-29 09:53:50 -0300

    Corrige rota Swagger no Render

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ from flasgger import Swagger
 
 app = Flask(__name__)
 
-Swagger(app)
+Swagger(app, config={
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec",
+            "route": "/apispec.json",
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+})
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
